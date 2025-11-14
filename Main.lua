7d8ac16a8cb6d16988662dfe8ba2dded3814cd4b
pakbeo <117583935+pakbeo@users.noreply.github.com>
2025-11-15 01:52:27 +0700

    Update webhook URL in Main.lua

--- Main.lua
+++ Main.lua
@@ -12,7 +12,7 @@ getgenv().lessPing = true --turn this on if u want lower ping servers, cant guar
 getgenv().autoRequiem = false --turn this on for auto requiem
 getgenv().NPCTimeOut = 15 --timeout for npc not spawning
 getgenv().HamonCharge = 90 --change if u want to charge hamon after every kill (around 90)
-getgenv().webhook = "https://discord.com/api/webhooks/1136054137096769591/nQTJFB_bwXt_err2nbS_BTjHsDCxtSCvFWOg9SQMGO7sqho279KtxoC1hPlxmLd8uJBa" --change this if u want to use ur own webhook
+getgenv().webhook = "https://discord.com/api/webhooks/1099327973590581318/phPrmef4a6LLaWFo8YnnfiBEg-aOEju3yodzBptQBWy8fWWKYy5g-AP89Rp97POBht9z" --change this if u want to use ur own webhook
 
 game:GetService("CoreGui").DescendantAdded:Connect(function(child)
     if child.Name == "ErrorPrompt" then

4157d841589d748f3ab4aeee21900c70d372851c
pakbeo <117583935+pakbeo@users.noreply.github.com>
2025-11-15 01:55:03 +0700

    Remove standList and global settings from Main.lua
    Removed the standList and various global settings from the script.

--- Main.lua
+++ Main.lua
@@ -1,19 +1,3 @@
-getgenv().standList =  {
-    ["The World"] = true,
-    ["Star Platinum"] = true,
-    ["Star Platinum: The World"] = true,
-    ["Crazy Diamond"] = true,
-    ["King Crimson"] = true,
-    ["King Crimson Requiem"] = true
-}
-getgenv().waitUntilCollect = 0.5 --Change this if ur getting kicked a lot
-getgenv().sortOrder = "asc" --desc for less players, asc for more
-getgenv().lessPing = true --turn this on if u want lower ping servers, cant guarantee you will see same people using script, and data error 1
-getgenv().autoRequiem = false --turn this on for auto requiem
-getgenv().NPCTimeOut = 15 --timeout for npc not spawning
-getgenv().HamonCharge = 90 --change if u want to charge hamon after every kill (around 90)
-getgenv().webhook = "https://discord.com/api/webhooks/1099327973590581318/phPrmef4a6LLaWFo8YnnfiBEg-aOEju3yodzBptQBWy8fWWKYy5g-AP89Rp97POBht9z" --change this if u want to use ur own webhook
-
 game:GetService("CoreGui").DescendantAdded:Connect(function(child)
     if child.Name == "ErrorPrompt" then
         local GrabError = child:FindFirstChild("ErrorMessage",true)
